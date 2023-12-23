a3b091958867212bac5bd0b13d987980e2990b8e
PaulHpsr <133357198+PaulHpsr@users.noreply.github.com>
2023-12-23 21:10:42 +0000

    Correction de l'obtention des arguments -> tokens bcp plus simple que la liste chainée

--- progc/Cy_Truck.c
+++ progc/Cy_Truck.c
@@ -1,39 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+//Headers
 #include "Outils_AVL.h"
 #include "Traitements.h"
 
 
+
 int main(int argc, char *argv[]) {
-    if (argc < 5) {
-        fprintf(stderr, "Usage: %s -o <option_choisie> -f <fichier>\n", argv[0]);
-        return 1;
-    }
-
-    // Récupérer les options
-    char *option = argv[2];  // -o option_choisie
-    char *fichier = argv[4]; // -f fichier
-
-    printf("Option choisie : %s\n", option);
-    printf("Fichier : %s\n", fichier);
-
-    // Traitement des options
-
-  if (strcmp(option, "-d1") == 0) {
-      traitement_d1(fichier);
-  } else if (strcmp(option, "-d2") == 0) {
-      traitement_d2(fichier);
-  } else if (strcmp(option, "-l") == 0) {
-      traitement_l(fichier);
-  } else if (strcmp(option, "-t") == 0) {
-      traitement_t(fichier);
-  } else if (strcmp(option, "-s") == 0) {
-      traitement_s(fichier);
-  } else {
-      fprintf(stderr, "Option invalide : %s\n", option);
-      return 1;
+  if (argc < 3) {                             //Vérifier le C à bien récupéré les arguments donnés par le shell (<3 car -> nom prog + option + chemin csv)
+    fprintf(stderr, "Erreur, pas assez d'arguments, ou mauvais arguments\n");
+    return 1;
   }
 
-    return 0;
+  // Récupérer les arguments dans des varr globales ?
+  char *chemin_csv = argv[1]; 
+  char *option_traitement = argv[2];
+  
+  //Diviser les options en plusieurs tokens
+  char *token = strtok(option_traitement, " "); //Ainsi on récupère les options séparées par des espaces
+  
+  // Traitement des options
+  while(token != NULL)         //Parcourir token pour effectuer chaques traitements
+    {
+      if(strcmp(token, "-d1") == 0)
+      {
+        traitement_d1(chemin_csv);
+      }
+      if(strcmp(token, "-d2") == 0)
+      {
+        traitement_d2(chemin_csv);
+      }
+      if(strcmp(token, "-l") == 0)
+      {
+        traitement_l(chemin_csv);
+      }
+      if(strcmp(token, "-t") == 0)
+      {
+        traitement_t(chemin_csv);
+      }
+      if(strcmp(token, "-s") == 0)
+      {
+        traitement_s(chemin_csv);
+      }
+
+      token = strtok(NULL, " ");  //On passe au token suivant
+    }
+
+
+
+//Fin du programme -> time
+return 0;
 }
